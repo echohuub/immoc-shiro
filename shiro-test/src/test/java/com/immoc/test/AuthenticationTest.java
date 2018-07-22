380ba65ef8564c61b37c01b18c45b7f83843d2d0
He Qingbao <qingbao.ho@gmail.com>
2018-07-22 11:57:42 +0800

    3-1 Shiro认证

---
+++ shiro-test/src/test/java/com/immoc/test/AuthenticationTest.java
@@ -0,0 +1,40 @@
+package com.immoc.test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.subject.Subject;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AuthenticationTest {
+
+    private SimpleAccountRealm realm = new SimpleAccountRealm();
+
+    @Before
+    public void addUser() {
+        realm.addAccount("Mark", "123456");
+    }
+
+    @Test
+    public void testAuthentication() {
+
+        // 1.构建SecurityManager环境
+        DefaultSecurityManager securityManager = new DefaultSecurityManager();
+        securityManager.setRealm(realm);
+
+        // 2.主体提交认证请求
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("Mark", "123456");
+        subject.login(token);
+
+        System.out.println("isAuthenticated: " + subject.isAuthenticated());
+
+        subject.logout();
+
+        System.out.println("isAuthenticated: " + subject.isAuthenticated());
+    }
+}
