65860eb0d35df707768f67d5ad679af779320eb5
He Qingbao <qingbao.ho@gmail.com>
2018-07-22 12:44:26 +0800

    3-3 IniRealm讲解

---
+++ shiro-test/src/test/java/com/immoc/test/IniRealmTest.java
@@ -0,0 +1,35 @@
+package com.immoc.test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.text.IniRealm;
+import org.apache.shiro.subject.Subject;
+import org.junit.Test;
+
+public class IniRealmTest {
+
+    @Test
+    public void testAuthentication() {
+
+        IniRealm iniRealm = new IniRealm("classpath:user.ini");
+
+        // 1.构建SecurityManager环境
+        DefaultSecurityManager securityManager = new DefaultSecurityManager();
+        securityManager.setRealm(iniRealm);
+
+        // 2.主体提交认证请求
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("Mark", "123456");
+        subject.login(token);
+
+        System.out.println("isAuthenticated: " + subject.isAuthenticated());
+
+        subject.checkRole("admin");
+
+        subject.checkPermission("user:delete");
+        subject.checkPermissions("user:delete", "user:update");
+    }
+}
