34f0c5619e1aa5763053afc53b04ad0326c5dd17
He Qingbao <qingbao.ho@gmail.com>
2018-07-22 17:47:10 +0800

    5-1 Shiro会话管理

---
+++ shiro-web/src/main/java/com/immoc/session/RedisSessionDao.java
@@ -0,0 +1,88 @@
+package com.immoc.session;
+
+import com.immoc.util.JedisUtil;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.UnknownSessionException;
+import org.apache.shiro.session.mgt.eis.AbstractSessionDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.SerializationUtils;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class RedisSessionDao extends AbstractSessionDAO {
+
+    @Autowired
+    private JedisUtil jedisUtil;
+
+    private static final String SHIRO_SESSION_PREFIX = "immoc-session:";
+
+    private byte[] getKey(String key) {
+        return (SHIRO_SESSION_PREFIX + key).getBytes();
+    }
+
+    private void saveSession(Session session) {
+        if (session != null && session.getId() != null) {
+            byte[] key = getKey(session.getId().toString());;
+            byte[] value = SerializationUtils.serialize(session);
+            jedisUtil.set(key, value);
+            jedisUtil.expire(key, 60);
+        }
+    }
+
+    @Override
+    protected Serializable doCreate(Session session) {
+        System.out.println("create session");
+        Serializable sessionId = generateSessionId(session);
+        assignSessionId(session, sessionId);
+        saveSession(session);
+        return sessionId;
+    }
+
+    @Override
+    protected Session doReadSession(Serializable serializable) {
+        System.out.println("read session");
+        if (serializable == null) {
+            System.out.println("read session null");
+            return null;
+        }
+
+        byte[] key = getKey(serializable.toString());
+        byte[] value = jedisUtil.get(key);
+        Session session = (Session) SerializationUtils.deserialize(value);;
+        System.out.println("read session " + session);
+        return session;
+    }
+
+    @Override
+    public void update(Session session) throws UnknownSessionException {
+        saveSession(session);
+    }
+
+    @Override
+    public void delete(Session session) {
+        if (session == null || session.getId() == null) {
+            return;
+        }
+        byte[] key = getKey(session.getId().toString());
+        jedisUtil.delete(key);
+    }
+
+    @Override
+    public Collection<Session> getActiveSessions() {
+        Set<byte[]> keys = jedisUtil.keys(SHIRO_SESSION_PREFIX);
+        Set<Session> sessions = new HashSet<>();
+        if (CollectionUtils.isEmpty(keys)) {
+            return sessions;
+        }
+
+        for (byte[] key : keys) {
+            Session session = (Session) SerializationUtils.deserialize(jedisUtil.get(key));
+            sessions.add(session);
+        }
+        return sessions;
+    }
+}
